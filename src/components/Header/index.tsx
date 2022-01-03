af247540f3cbfdd9325cd43eb127d34eea2b047a
Samuel Alves <samuellmsl05@gmail.com>
2022-01-03 14:45:32 -0300

    :art: Componente Summary na Dashboard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,6 @@ export function Header() {
     return (
         <Container>
             <Content>
-
                 <img src={logoImg} alt="dtmoney" />
                 <button type="button">
                     Nova transação
